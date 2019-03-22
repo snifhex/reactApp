187c39e8ecff22c32b4ccdc830f77600c51a7635
Sachin Tripathi <sachintripathi@protonmail.com>
2019-03-23 00:50:07 +0530

    Updates

---
+++ precache-manifest.c5fa73529f0ffc0e8d92a5770d64915e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b7da2cc7b0b862644ad",
+    "url": "/reactapp/static/css/main.b32ba7eb.chunk.css"
+  },
+  {
+    "revision": "0b7da2cc7b0b862644ad",
+    "url": "/reactapp/static/js/main.cb8368e4.chunk.js"
+  },
+  {
+    "revision": "699d48caefa08891bb24",
+    "url": "/reactapp/static/js/runtime~main.7f5faac6.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/reactapp/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92b117edd7ce815a4d96b85214f21d54",
+    "url": "/reactapp/index.html"
+  }
+];
